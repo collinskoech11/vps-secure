be2e24b4c6e0e212b9cd7ffe5d0f26bd8f81bf49
collinskoech11 <collinskoechck34@gmail.com>
2025-11-21 12:46:47 +0300

    updated allowed_hosts, csrf_trusted_origins, cors_allow_all_origins

--- vpssecureserver/settings.py
+++ vpssecureserver/settings.py
@@ -128,6 +128,10 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*']
+CORS_ALLOW_ALL_ORIGINS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
